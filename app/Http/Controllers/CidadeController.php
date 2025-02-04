ad5976dea7579704d029a65c7a21efea2d09070f
João Marcelo <marcelodjmix2013@gmail.com>
2025-02-04 18:41:51 -0300

    ajuste para filtros por nome

--- app/Http/Controllers/CidadeController.php
+++ app/Http/Controllers/CidadeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cidade;
+use Illuminate\Http\Request;
 
 class CidadeController extends Controller {
 
-    public function index() {
-        return response()->json(Cidade::all());
+    public function index(Request $request) {
+        
+        $nome = $request->query('nome');
+
+        $cidades = Cidade::when($nome, function ($query, $nome) {
+            $query->where('nome', 'LIKE', "%{$nome}%");
+        })->orderBy('nome')->get();
+
+        return response()->json($cidades);
     }
-
-
 }
--- app/Http/Controllers/MedicoController.php
+++ app/Http/Controllers/MedicoController.php
@@ -7,16 +7,42 @@ use Illuminate\Http\Request;
 
 class MedicoController extends Controller {
     
-    public function index() {
-        return response()->json(Medico::with('cidade')->get());
+    public function index(Request $request) {
+
+        $nome = $request->query('nome');
+
+        $medicos = Medico::with('cidade')
+            ->when($nome, function ($query, $nome) {
+                $query->whereRaw("REPLACE(REPLACE(nome, 'Dr. ', ''), 'Dra. ', '') LIKE ?", ["%{$nome}%"]);
+            })
+            ->orderBy('nome')
+            ->get();
+
+        return response()->json($medicos);
+    }
+
+    public function getMedicosByCidade(Request $request, $id_cidade) {
+
+        $nome = $request->query('nome');
+
+        $medicos = Medico::where('cidade_id', $id_cidade)
+            ->when($nome, function ($query, $nome) {
+                $query->whereRaw("REPLACE(REPLACE(nome, 'Dr. ', ''), 'Dra. ', '') LIKE ?", ["%{$nome}%"]);
+            })
+            ->orderBy('nome')
+            ->get();
+
+        return response()->json($medicos);
     }
 
     public function store(Request $request) {
+
         $medico = Medico::create($request->validate([
             'nome' => 'required|string|max:255',
             'especialidade' => 'required|string|max:255',
             'cidade_id' => 'required|exists:cidades,id',
         ]));
+
         return response()->json($medico, 201);
     }
 
